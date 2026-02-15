73e054dcf84d13f5f23942c86925cf10f0292dc5
Ian Low <ian.low.weimin@gmail.com>
2026-02-15 16:11:31 +0800

    Add storage of inputs in a list which can be printed using cmd list

--- src/main/java/Late.java
+++ src/main/java/Late.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Late {
     /**
-     * Echoes an input and exits if input is "bye".
+     * Echoes text inputs and saves them in a list
+     * Prints the list if input is "list"
+     * Exits if input is "bye"
      * @param args text input from user
      */
     public static void main(String[] args) {
@@ -12,6 +16,7 @@ public class Late {
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
+        List<String> userInputs = new ArrayList<>();
 
         while (true) {
             String input = scanner.nextLine();
@@ -23,8 +28,20 @@ public class Late {
                 break;
             }
 
+            if (input.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                int i = 1;
+                for (String item : userInputs) {
+                    System.out.println(i + ". " + item);
+                    i++;
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            }
+
+            userInputs.add(input);
             System.out.println("____________________________________________________________");
-            System.out.println(input);
+            System.out.println("added: " + input);
             System.out.println("____________________________________________________________");
         }
     }
